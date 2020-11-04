3cbc4c8c58049224e8c2ea378c6cc7cd7201a37d
Krzysztof Rutana <krzysztofrutana@wp.pl>
2020-11-04 17:47:04 +0100

    Create field and method to set or change job scheduler time interval.

--- app/src/main/java/com/example/przypominajka/fragments/settings/SettingFragment.java
+++ app/src/main/java/com/example/przypominajka/fragments/settings/SettingFragment.java
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +23,7 @@ import com.example.przypominajka.activities.AddNewEventActivity;
 import com.example.przypominajka.databases.PrzypominajkaDatabaseHelper;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
 import org.joda.time.LocalTime;
@@ -39,13 +41,15 @@ public class SettingFragment extends Fragment {
     private TextView defaultTimeField;
     private String defaultTime;
 
-    TimePickerDialog timePickerDefaultTime;
+    private TextView eventRefreshIntervalField;
+    private String eventRefreshInterval;
 
+    TimePickerDialog timePickerDefaultTime;
+    TimePickerDialog timePickerCheckInterval;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_setting, container, false);
     }
 
@@ -55,9 +59,9 @@ public class SettingFragment extends Fragment {
         przypominajkaDatabaseHelper = new PrzypominajkaDatabaseHelper(requireContext());
         defaultTimeField = view.findViewById(R.id.textSettingsDefaultTime);
         if (przypominajkaDatabaseHelper.getDefaultTime() == 0) {
-            defaultTimeField.setText("Wybierz godzinę");
+            defaultTimeField.setText("wybierz godzinę");
         } else {
-            LocalTime time = new LocalTime(przypominajkaDatabaseHelper.getDefaultTime());
+            LocalTime time = new LocalTime(przypominajkaDatabaseHelper.getDefaultTime(), DateTimeZone.forID("Etc/Universal"));
             defaultTimeField.setText(time.toString(DateTimeFormat.forPattern("HH:mm")));
         }
         defaultTimeField.setOnClickListener(new View.OnClickListener() {
@@ -71,17 +75,60 @@ public class SettingFragment extends Fragment {
                 timePickerDefaultTime = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        String time = selectedHour + ":" + selectedMinute;
+                        String minute = "";
+                        if (selectedMinute < 10) {
+                            minute = "0" + selectedMinute;
+                        } else {
+                            minute = String.valueOf(selectedMinute);
+                        }
+                        String time = selectedHour + ":" + minute;
                         defaultTimeField.setText(time);
                         defaultTimeField.setTextSize(18);
                         LocalTime date = new LocalTime(selectedHour, selectedMinute);
                         przypominajkaDatabaseHelper.updateDefaultTimeInSettings(date.getMillisOfDay());
                     }
-                }, hour, minute, true);//Yes 24 hour time
+                }, hour, minute, DateFormat.is24HourFormat(requireContext()));//Yes 24 hour time
                 timePickerDefaultTime.setTitle("Wybierz godzinę");
                 timePickerDefaultTime.show();
             }
         });
+
+        eventRefreshIntervalField = view.findViewById(R.id.textCheckEventInerval);
+        if (przypominajkaDatabaseHelper.getCheckEventInterval() == 0) {
+            eventRefreshIntervalField.setText("wybierz czas");
+        } else {
+            LocalTime time = new LocalTime(przypominajkaDatabaseHelper.getCheckEventInterval(), DateTimeZone.forID("Etc/Universal"));
+            eventRefreshIntervalField.setText(time.toString(DateTimeFormat.forPattern("HH:mm")));
+        }
+        eventRefreshIntervalField.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Use the current time as the default values for the picker
+                final Calendar c = Calendar.getInstance();
+                int hour = 6;
+                int minute = 0;
+                // Create a new instance of c
+                timePickerCheckInterval = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        String minute = "";
+                        if (selectedMinute < 10) {
+                            minute = "0" + selectedMinute;
+                        } else {
+                            minute = String.valueOf(selectedMinute);
+                        }
+                        String time = selectedHour + ":" + minute;
+                        eventRefreshIntervalField.setText(time);
+                        eventRefreshIntervalField.setTextSize(18);
+                        LocalTime date = new LocalTime(selectedHour, selectedMinute);
+                        przypominajkaDatabaseHelper.updateCheckEventInterval(date.getMillisOfDay());
+                    }
+                }, hour, minute, true);
+                timePickerCheckInterval.setTitle("Wybierz czas");
+                timePickerCheckInterval.show();
+            }
+        });
+
         super.onViewCreated(view, savedInstanceState);
     }
 
